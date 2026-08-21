aecfca72d85a9cc14c4c12b70591a4a623671233
aryan21231212 <aryanpratapsingh674@gmail.com>
2026-08-21 18:01:52 +0530

    changes made

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,7 +4,7 @@ config();
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || '');
+    const conn = await mongoose.connect("mongodb+srv://aryanpratapsingh862:83oSj0psXp1taAF7@cluster1.8ynb5tf.mongodb.net/campusos?appName=Cluster1");
     console.log(`[Database] MongoDB Atlas Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('[Database Error] Connection failed:', error);
